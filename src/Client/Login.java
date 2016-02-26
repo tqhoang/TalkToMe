4156078cf73a2f247e16523b0c9ac7ff085222a6
Trung Hoang <tqhoang.ca@gmail.com>
2016-02-26 00:17:30 -0800

    Created basic variables for login class

--- src/Client/Login.java
+++ src/Client/Login.java
@@ -7,6 +7,27 @@
 
 package Client;
 
-public class Login {
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
+@SuppressWarnings("serial")
+public class Login extends JFrame {
+	// Login class UI variables
+	private String TITLE = "TalkToMe - LOGIN";
+	private int WIDTH = 400;
+	private int HEIGHT = 300;
+	
+	// User variables
+	static String USERNAME;
+	
+	// Connection variables
+	static int PORT;
+	private JTextField username;
+	private JTextField server;
+	private JTextField port;
+	
+			
+	public Login() {
+		
+	}
 }
